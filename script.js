131c559b466732ac9b74890d38d6b2caf9a33904
Adarsh Sharma <110263814+okadarsh@users.noreply.github.com>
2024-03-26 00:01:38 +0530

    major project yhill

---
+++ script.js
@@ -0,0 +1,29 @@
+// Smooth scrolling effect for navigation links
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+// Form validation
+function validateForm() {
+  var name = document.forms["contactForm"]["name"].value;
+  var email = document.forms["contactForm"]["email"].value;
+  var message = document.forms["contactForm"]["message"].value;
+  if (name == "" || email == "" || message == "") {
+    alert("Please fill in all fields.");
+    return false;
+  }
+}
